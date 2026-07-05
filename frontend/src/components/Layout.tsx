29a2ce927e0909380b24b98b614820e100ee36eb
marastac <132803403+marastac@users.noreply.github.com>
2026-07-05 13:06:07 -0500

    update layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactPixel from 'react-facebook-pixel';
 import { Header } from './Header';
 
 interface Props {
   children: React.ReactNode;
 }
 
-export const Layout: React.FC<Props> = ({ children }) => (
-  <div className="page">
-    <Header />
-    <main>{children}</main>
-    <footer className="foot">
-      Your peace of mind is our priority. We are with you every step of the way.
-    </footer>
-  </div>
-);
+export const Layout: React.FC<Props> = ({ children }) => {
+  useEffect(() => {
+    // Inicialización del Pixel de Meta
+    const PIXEL_ID = '1282637307096201';
+    ReactPixel.init(PIXEL_ID);
+    
+    // Rastrea la página cuando se carga el layout
+    ReactPixel.pageView();
+  }, []);
+
+  return (
+    <div className="page">
+      <Header />
+      <main>{children}</main>
+      <footer className="foot">
+        Your peace of mind is our priority. We are with you every step of the way.
+      </footer>
+    </div>
+  );
+};
